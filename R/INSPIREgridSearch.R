8e9cc4db2c4d2d6c8c6322ed8c0b5ad2ac79c92c
Ben Logsdon <blogsdon@c02vp283hv2q.fhcrc.org>
2017-12-21 14:51:24 -0800

    get rid of parallelization

--- R/INSPIREgridSearch.R
+++ R/INSPIREgridSearch.R
@@ -2,14 +2,10 @@ INSPIREgridSearch <- function(Xq,
                               mcnt = seq(10,200,by=40),
                               lambda = 10^seq(-2,3,by=1)){
   res1 <- expand.grid(mcnt,lambda)
-  no_cores <- parallel::detectCores()
-  cl <- parallel::makeCluster(no_cores)
-  res1$bic<- parallel::clusterMap(cl,
-                                  INSPIREoptimizer::INSPIREbicWrapper,
+  res1$bic<- parallel::mapply(INSPIREoptimizer::INSPIREbicWrapper,
                                   res1$Var1,
                                   res1$Var2,
                                   MoreArgs = list(X=Xq),
                                   SIMPLIFY=TRUE)
-  parallel::stopCluster(cl)
   return(res1)
 }
