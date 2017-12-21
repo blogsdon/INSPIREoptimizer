57e4390969b34688af40185d1f9718b99829f0e4
Ben Logsdon <blogsdon@c02vp283hv2q.fhcrc.org>
2017-12-21 14:53:42 -0800

    fix bug

--- R/INSPIREgridSearch.R
+++ R/INSPIREgridSearch.R
@@ -2,7 +2,7 @@ INSPIREgridSearch <- function(Xq,
                               mcnt = seq(10,200,by=40),
                               lambda = 10^seq(-2,3,by=1)){
   res1 <- expand.grid(mcnt,lambda)
-  res1$bic<- parallel::mapply(INSPIREoptimizer::INSPIREbicWrapper,
+  res1$bic<- mapply(INSPIREoptimizer::INSPIREbicWrapper,
                                   res1$Var1,
                                   res1$Var2,
                                   MoreArgs = list(X=Xq),
