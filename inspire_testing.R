d9f9fd005e2d863484813ebb877e6643947e2236
Ben Logsdon <blogsdon@c02vp283hv2q.fhcrc.org>
2017-12-22 14:26:20 -0800

    updated testing

--- inspire_testing.R
+++ inspire_testing.R
@@ -1,7 +1,7 @@
-mcnt = 10
-lambda = .1
+mcnt = 170
+lambda = .001
 data('two_example_datasets')
-X <- list(x1 = scale(log(exmp_dataset1)),
+X2 <- list(x1 = scale(log(exmp_dataset1)),
           x2 = scale(log(exmp_dataset2)))
 res = INSPIRE(X, mcnt, lambda)
 
@@ -12,3 +12,22 @@ modComplex <- INSPIREoptimizer::extractModelComplexityParameters(X,res$L,res$Z,r
 bicTest <- INSPIREoptimizer::INSPIREbic(X,res$L,res$Z,res$theta,lambda)
 
 optimizedINSPIRE <- INSPIREoptimizer::INSPIREgridSearch(X)
+foobar<-tidyr::spread(optimizedINSPIRE,key='Var1',value='bic')
+
+
+synapseClient::synapseLogin()
+X <- rSynapseUtilities::loadDelimIntoList(c('syn11275934','syn11275945'))
+cleanData <- function(x){
+  rownames(x) <- x[,1]
+  x <- x[,-1]
+  x <- t(x)
+  x <- apply(x,2,utilityFunctions::winsorize)
+  x <- scale(x)
+  return(x)
+}
+X <- lapply(X,cleanData)
+
+subSampleX <- sample(1:20514,2000)
+X3 <- lapply(X,function(x,y) x[,y],subSampleX)
+optimizedINSPIRE <- INSPIREoptimizer::INSPIREgridSearch(X3)
+
